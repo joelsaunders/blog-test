9705f04ad0de25261a01cc0a387fbf4584b0c2a0
Joel Saunders <jsaunders155@hotmail.com>
2016-04-09 22:13:33 +0100

    added posts to main page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Post #import the model created previously
+from django.utils import timezone #import timezone for sorting
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    # order posts and exclude unpublished posts
+    posts = Post.objects.filter(published_date__lte=
+        timezone.now()).order_by('published_date')
+    #render html page
+    return render(request, 'blog/post_list.html', {'posts': posts})
