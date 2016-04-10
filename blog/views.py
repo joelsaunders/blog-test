68927d015550610fc40ddc5d9b6895b043a27811
Joel Saunders <jsaunders155@hotmail.com>
2016-04-10 09:06:33 +0100

    added focus view for posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post #import the model created previously
 from django.utils import timezone #import timezone for sorting
 
@@ -9,3 +9,8 @@ def post_list(request):
         timezone.now()).order_by('published_date')
     #render html page
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    #add view for post details pages
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
